48be8efe313c5bfc3f7d9b7c0bb810005860c737
hunaric <nathanaelfangnon@gmail.com>
2025-10-30 00:29:08 +0100

    Header et sidebar presque acheves

--- front/src/app/shared/layout/app-header/app-header.component.ts
+++ front/src/app/shared/layout/app-header/app-header.component.ts
@@ -1,11 +1,13 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 import { SidebarService } from '../../services/sidebar.service';
+import { ThemeToggleComponent } from '../../components/common/theme-toggle/theme-toggle.component';
 
 @Component({
   selector: 'app-app-header',
   imports: [
-    CommonModule
+    CommonModule,
+    ThemeToggleComponent,
   ],
   templateUrl: './app-header.component.html',
   styleUrl: './app-header.component.css'
@@ -13,6 +15,9 @@ import { SidebarService } from '../../services/sidebar.service';
 export class AppHeaderComponent {
   isApplicationMenuOpen = false;
   readonly isMobileOpen$;
+
+
+  @ViewChild('searchInput') searchInput!: ElementRef<HTMLInputElement>;
 
   constructor(public sidebarService: SidebarService) {
     this.isMobileOpen$ = this.sidebarService.isMobileOpen$;
@@ -29,5 +34,21 @@ export class AppHeaderComponent {
   toggleApplicationMenu() {
     this.isApplicationMenuOpen = !this.isApplicationMenuOpen;
   }
+
+
+  ngAfterViewInit() {
+    document.addEventListener('keydown', this.handleKeyDown);
+  }
+
+  ngOnDestroy() {
+    document.removeEventListener('keydown', this.handleKeyDown);
+  }
+
+  handleKeyDown = (event: KeyboardEvent) => {
+    if ((event.metaKey || event.ctrlKey) && (event.key === 'q' || event.key === 'Q')) {
+      event.preventDefault();
+      this.searchInput?.nativeElement.focus();
+    }
+  };
 
 }
