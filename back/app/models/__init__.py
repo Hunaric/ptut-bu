9fdbc47f54fbaad0c71b9056d5698641a520f1a6
hunaric <nathanaelfangnon@gmail.com>
2025-12-14 08:00:43 +0100

    apres upload de ma branche annexe

--- back/app/models/__init__.py
+++
@@ -1,6 +0,0 @@
-# app/models/__init__.py
-from .user import User
-from .role import Role
-from .account import Account
-from .permission import Permission
-# from .role_permission import RolePermission 
